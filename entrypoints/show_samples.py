04e1818848784014cf82c58e889964e1ffccdf36
Luke Wood <lukewoodcs@gmail.com>
2022-04-05 19:34:05 -0700

    Create tf.data loader for images and annotations

--- entrypoints/show_samples.py
+++ entrypoints/show_samples.py
@@ -1,3 +1,4 @@
+import cv2
 import matplotlib.pyplot as plt
 import wandb
 from absl import app
@@ -13,13 +14,25 @@ FLAGS = flags.FLAGS
 config_flags.DEFINE_config_file("config", "configs/main.py")
 
 
+def visualize_bounding_boxes(img, annotations):
+    for annotation in annotations:
+        x, y, w, h, category = annotation.astype(int)
+        if h == -1:
+            break
+        img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
+    return img
+
+
 def main(args):
     config = FLAGS.config
     ds = reef_net.loaders.load_reef_dataset(config)
+    ds = ds.shuffle(100)
 
     (image, labels) = next(iter(ds.take(1)))
     image, labels = image.numpy(), labels.numpy()
-    plt.imshow(image/255.0)
+
+    image = visualize_bounding_boxes(image, labels)
+    plt.imshow(image / 255.0)
     plt.show()
 
 
--- reef_net/loaders/reef_loader.py
+++ reef_net/loaders/reef_loader.py
@@ -20,16 +20,17 @@ def get_bboxes(annotations):
         result.append(box)
     return result
 
+
 def pad_to_shape(a, shape):
     for _ in range(len(shape) - len(a.shape)):
         a = np.expand_dims(a, axis=0)
     y_, x_ = shape
     y, x = a.shape
-    y_pad = (y_-y)
-    x_pad = (x_-x)
-    return np.pad(a,((0, y_pad),
-                     (0, x_pad)),
-                  mode = 'constant', constant_values=-1)
+    y_pad = y_ - y
+    x_pad = x_ - x
+    return np.pad(a, ((0, y_pad), (0, x_pad)), mode="constant", constant_values=-1)
+
+
 #
 # def parse_annotations(annotations, max_boxes):
 #     result = []
