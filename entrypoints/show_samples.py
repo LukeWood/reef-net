56cb33a2b85d42749f0e2aff6cf43d5bec0f258e
Luke Wood <lukewoodcs@gmail.com>
2022-04-29 11:50:00 -0700

    Update

--- entrypoints/show_samples.py
+++ entrypoints/show_samples.py
@@ -5,8 +5,9 @@ from absl import app
 from absl import flags
 from absl import logging
 from ml_collections.config_flags import config_flags
-
+from reef_net.preprocess import preprocess_data
 import reef_net
+import numpy as np
 
 FLAGS = flags.FLAGS
 
@@ -16,10 +17,14 @@ config_flags.DEFINE_config_file("config", "configs/main.py")
 
 def visualize_bounding_boxes(img, annotations, category):
     for annotation in annotations:
-        x, y, w, h = annotation.astype(int)
+        x, y, w, h = annotation.astype(np.float32)
+        x1 = (x-w/2).astype(int)
+        x2 = (x+w/2).astype(int)
+        y1 = (y-h/2).astype(int)
+        y2 = (y+h/2).astype(int)
         if category == -1:
             break
-        img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
+        img = cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
     return img
 
 
@@ -27,10 +32,11 @@ def main(args):
     config = FLAGS.config
     ds = reef_net.loaders.load_reef_dataset(config, min_boxes_per_image=5)
     ds = ds.shuffle(20)
+    ds = ds.map(preprocess_data)
 
     (image, bounding_boxes, category) = next(iter(ds.take(1)))
     image, bounding_boxes, category = image.numpy(), bounding_boxes.numpy(), category.numpy()
-
+    print('bounding_boxes', bounding_boxes)
     image = visualize_bounding_boxes(image, bounding_boxes, category)
     plt.imshow(image / 255.0)
     plt.axis('off')
