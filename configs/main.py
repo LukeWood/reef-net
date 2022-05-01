97ebbdd5d349bf52a846ab31865e446d7134936d
Luke <lukewoodcs@gmail.com>
2022-05-01 12:32:08 -0700

    Remove unneeded files, cleanup work

--- configs/main.py
+++ configs/main.py
@@ -3,6 +3,7 @@ import ml_collections
 
 def get_config() -> ml_collections.ConfigDict:
     config = ml_collections.ConfigDict()
+    config.wandb_project_name = "reef-net"
     config.data_path = "tensorflow-great-barrier-reef"
     config.batch_size = 2
     config.input_shape = (720, 1280, 3)
