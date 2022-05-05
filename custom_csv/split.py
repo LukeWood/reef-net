cf9bc05be8c24f8e2773332e1ba449142866ac83
Luke Wood <lukewoodcs@gmail.com>
2022-05-04 22:28:35 -0700

    Add train test split files

---
+++ custom_csv/split.py
@@ -0,0 +1,24 @@
+import random
+
+with open("all.csv", "r") as f:
+    lines = f.readlines()
+
+header = lines[0]
+lines = lines[1:]
+total_lines = len(lines)
+
+indices = range(total_lines)
+test_indices = random.sample(indices, 2000)
+test_set = set(test_indices)
+train_indices = [line for line in indices if line not in test_set]
+
+with open("train.csv", "w") as train:
+    train.write(header)
+    for line in train_indices:
+        train.write(lines[line])
+
+with open("test.csv", "w") as test:
+    test.write(header)
+    for line in test_indices:
+        test.write(lines[line])
+
