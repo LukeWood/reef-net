a22a20ea45512aaa4e4fbe03bb588f7f0c5b5542
harry-kt <Harsh.Kishorsingh.Thakur@gmail.com>
2022-05-05 16:06:17 -0700

    Test Val Path Added

--- configs/main.py
+++ configs/main.py
@@ -5,6 +5,9 @@ def get_config() -> ml_collections.ConfigDict:
     config = ml_collections.ConfigDict()
     config.wandb_project_name = "reef-net"
     config.data_path = "tensorflow-great-barrier-reef"
+    config.custom_path = "custom_csv"
+    config.train_path = "train.csv"
+    config.val_path = "val.csv"
     config.batch_size = 2
     config.num_classes = 1
     config.input_shape = (720, 1280, 3)
