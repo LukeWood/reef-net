f1ff6e361a2a1cbdbf070cc3a5ef41c74a00c683
Luke Wood <lukewoodcs@gmail.com>
2022-06-09 19:38:02 +0000

    add create-training-video.sh

---
+++ shell/create-training-video.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm -rf make_video/
+mkdir make_video
+cp -r $1 make_video/
+rm make_video/ground_truth.png
+name=${2:-learning}
+ffmpeg -framerate 1 -pattern_type glob -i "make_video/*.png" media/$name.mp4
