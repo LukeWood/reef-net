42835ccde9b433ed32598a7d58d4334f8198dd94
Luke Wood <lukewoodcs@gmail.com>
2022-06-09 20:04:56 +0000

    updates

--- shell/create-training-video.sh
+++ shell/create-training-video.sh
@@ -8,3 +8,4 @@ python shell/pad-image-names.py
 rm make_video/ground_truth.png
 name=${2:-learning}
 ffmpeg -framerate 2 -pattern_type glob -i "make_video/*.png" -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -pix_fmt yuv420p media/$name.mp4
+ffmpeg -framerate 2 -pattern_type glob -i "make_video/*.png" -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" media/$name.gif
