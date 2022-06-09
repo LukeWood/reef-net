c7705ac9c8c107111f514b81489907da5f76fc1c
Luke Wood <lukewoodcs@gmail.com>
2022-06-09 19:51:08 +0000

    Update video

--- shell/create-training-video.sh
+++ shell/create-training-video.sh
@@ -1,9 +1,10 @@
-bin/sh
+#!/bin/sh
 
 set -x
 rm -rf make_video/
 mkdir make_video
 cp $1/* make_video/
+python shell/pad-image-names.py
 rm make_video/ground_truth.png
 name=${2:-learning}
-ffmpeg -framerate 1 -pattern_type glob -i "make_video/*.png" media/$name.mp4
+ffmpeg -framerate 1 -i "make_video/%03d.png" media/$name.mp4
