074bb72f9eaf0a4384583dcae4722c166b588ea4
Luke Wood <lukewoodcs@gmail.com>
2022-06-09 19:53:45 +0000

    Fix issues

--- shell/create-training-video.sh
+++ shell/create-training-video.sh
@@ -7,4 +7,5 @@ cp $1/* make_video/
 python shell/pad-image-names.py
 rm make_video/ground_truth.png
 name=${2:-learning}
-ffmpeg -framerate 1 -i "make_video/%03d.png" media/$name.mp4
+ffmpeg -framerate 1 -i "make_video/%03d.png" -vcodec libx264 \
+  -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" media/$name.mp4
