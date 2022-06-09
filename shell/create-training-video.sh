1925822733cd47953c86e92ba53dcac7719ad939
Luke Wood <lukewoodcs@gmail.com>
2022-06-09 19:59:42 +0000

    Update to 2 fps

--- shell/create-training-video.sh
+++ shell/create-training-video.sh
@@ -7,4 +7,4 @@ cp $1/* make_video/
 python shell/pad-image-names.py
 rm make_video/ground_truth.png
 name=${2:-learning}
-ffmpeg -framerate 1 -pattern_type glob -i "make_video/*.png" -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -pix_fmt yuv420p media/$name.mp4
+ffmpeg -framerate 2 -pattern_type glob -i "make_video/*.png" -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -pix_fmt yuv420p media/$name.mp4
